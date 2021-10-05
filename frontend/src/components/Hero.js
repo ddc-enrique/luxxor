f7be83c82919a38436aff4d5e52910bd4fcd77f8
micaela-torre <micaelatorre@outlook.es>
2021-10-05 13:14:30 -0300

    header, hero y footer

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,10 +1,11 @@
+import Carousel from '../components/Carousel'
 import styles from '../styles/hero.module.css'
 
 const Hero=()=>{
     return(
         <div className={styles.heroContainer}>
             <div className={styles.containerProducts}  style={{backgroundImage: 'url("https://i.postimg.cc/FK1H0bkM/Nombre-4.png")'}}>
-                <div className={styles.productPic} style={{backgroundImage: 'url("https://i.postimg.cc/sg5jwZQH/Nombre-5.png")'}}></div>
+            <Carousel/> 
             </div>
             <div className={styles.subtitleHero}>
                 <h2>
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,6 +1,12 @@
+import { useState } from 'react'
 import styles from '../styles/navBar.module.css'
 
 const NavBar = () => {
+    const [visible, setVisible] =useState(false)
+    const clickHandler= ()=>{
+        setVisible(!visible)
+    }
+
 return(
     <header className={styles.headerContainer}>
         <h1>Lu<span className={styles.orange}>x</span><span className={styles.violet}>x</span>or</h1>
@@ -10,11 +16,15 @@ return(
             <p>Contacto</p>
         </nav>
         <div className={styles.navIcons}>
-            <div className={styles.icon} style={{backgroundImage: 'url("https://i.postimg.cc/jjnwNZtm/Dise-o-sin-t-tulo-44.png")'}}>
+            <div className={styles.icon} style={{backgroundImage: 'url("https://i.postimg.cc/jjnwNZtm/Dise-o-sin-t-tulo-44.png")'}} onClick={clickHandler}>
             </div>
             <div className={styles.icon} style={{backgroundImage: 'url("https://i.postimg.cc/1z2c686R/Dise-o-sin-t-tulo-46.png")'}}>
             </div>
             </div>
+            { visible &&  <div className={styles.dropDown}>
+                <p>Ingresar</p>
+                <p>Registrarme</p>
+                </div>}
     </header>
 )
 }
