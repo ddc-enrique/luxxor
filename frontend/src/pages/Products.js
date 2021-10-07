8bc79194162a4ab5dc7b04ee277a5d0c47e848d1
Fernando Leniz <ferleniz93@gmail.com>
2021-10-06 21:02:50 -0300

    products page finished

--- frontend/src/pages/Products.js
+++ frontend/src/pages/Products.js
@@ -1,10 +1,13 @@
-import React from "react"
+import React, { useState } from "react"
 import Product from "../components/Product"
 import Navbar from "../components/NavBar"
 import Footer from "../components/Footer"
 import styles from "../styles/productList.module.css"
+import Switch from "react-switch"
+import { Link } from "react-router-dom"
 
 const Products = () => {
+  const [checked, setChecked] = useState(false)
   const data = [
     {
       name: "Notebook",
@@ -27,6 +30,10 @@ const Products = () => {
     },
   ]
 
+  const handleChange = (checked) => {
+    setChecked(checked)
+  }
+
   return (
     <>
       <Navbar />
@@ -35,41 +42,125 @@ const Products = () => {
           <h2>Filtrar por:</h2>
           <div className={styles.inputsFilter}>
             <h3>Marca</h3>
-            <input
+            <div className={styles.switch}>
+              <label>
+                <span>Option 1</span>
+                <Switch
+                  onChange={handleChange}
+                  checked={checked}
+                  uncheckedIcon={false}
+                  checkedIcon={false}
+                  onColor={'#f48f31'}
+                />
+              </label>
+            </div>
+            <div className={styles.switch}>
+              <label>
+                <span>Option 1</span>
+                <Switch
+                  onChange={handleChange}
+                  checked={checked}
+                  uncheckedIcon={false}
+                  checkedIcon={false}
+                  onColor={'#f48f31'}
+                />
+              </label>
+            </div>
+            <div className={styles.switch}>
+              <label>
+                <span>Option 1</span>
+                <Switch
+                  onChange={handleChange}
+                  checked={checked}
+                  uncheckedIcon={false}
+                  checkedIcon={false}
+                  onColor={'#f48f31'}
+                />
+              </label>
+            </div>
+
+            {/* <input
               type="text"
               // className={styles.inputTypes}
               placeholder="Marca"
               name="marca"
-            />
+            /> */}
             <h3>Categoria</h3>
-            <input
-              type="text"
-              // className={styles.inputTypes}
-              placeholder="categoria"
-              name="categoria"
-            />
-            <h3>Precio</h3>
-            <select
-              name="precio"
-              // value={}
-              // className=""
-              placeholder="precio"
-            >
-              <option value="volvo">Volvo</option>
-              <option value="saab">Saab</option>
-              <option value="mercedes">Mercedes</option>
-              <option value="audi">Audi</option>
-            </select>
+            <div className={styles.switch}>
+              <label>
+                <span>Option 1</span>
+                <Switch
+                  onChange={handleChange}
+                  checked={checked}
+                  uncheckedIcon={false}
+                  checkedIcon={false}
+                  onColor={'#f48f31'}
+                />
+              </label>
+            </div>
+            <div className={styles.switch}>
+              <label>
+                <span>Option 1</span>
+                <Switch
+                  onChange={handleChange}
+                  checked={checked}
+                  uncheckedIcon={false}
+                  checkedIcon={false}
+                  onColor={'#f48f31'}
+                />
+              </label>
+            </div>
           </div>
         </div>
         <div className={styles.productsSection}>
-          <h2>PRODUCTOS</h2>
+          <div className={styles.inputSelect}>
+            <div>
+            <h2>Ordenar por:</h2>
+            <select
+              name="precio"
+              // value={}
+              className={styles.selectInput}
+              placeholder="precio"
+            >
+              <option value="volvo"  default>Selecciona</option>
+              <option value="volvo">Mayor precio</option>
+              <option value="saab">Menor precio</option>
+              <option value="mercedes">A-Z</option>
+              <option value="audi">Z-A</option>
+            </select>
+            </div>
+          </div>
           <div className={styles.containerProducts}>
             <div className={styles.cardProduct}>
               <div className={styles.containPrice}>
                 <p>$282000</p>
                 <div>
-                <span>Off</span>
+                  <span>Off</span>
+                  <p>%10</p>
+                </div>
+              </div>
+              <p>
+                MacBook Air 13.3 Apple M1 8GB 512GB SSD MacOS X 11 Space Gray
+              </p>
+              <div className={styles.center}>
+                <div
+                  className={styles.photo}
+                  style={{
+                    backgroundImage: `url("https://home.ripley.com.pe/Attachment/WOP_5/2004209413829/2004209413829-1.jpg")`,
+                  }}
+                ></div>
+              </div>
+              <div className={styles.center}>
+                <Link to="/producto">
+                  <p className={styles.btnViewMore}>Ver +</p>
+                </Link>
+              </div>
+            </div>
+            <div className={styles.cardProduct}>
+              <div className={styles.containPrice}>
+                <p>$282000</p>
+                <div>
+                  <span>Off</span>
                   <p>%10</p>
                 </div>
               </div>
@@ -92,7 +183,7 @@ const Products = () => {
               <div className={styles.containPrice}>
                 <p>$282000</p>
                 <div>
-                <span>Off</span>
+                  <span>Off</span>
                   <p>%10</p>
                 </div>
               </div>
@@ -115,7 +206,7 @@ const Products = () => {
               <div className={styles.containPrice}>
                 <p>$282000</p>
                 <div>
-                <span>Off</span>
+                  <span>Off</span>
                   <p>%10</p>
                 </div>
               </div>
@@ -138,7 +229,7 @@ const Products = () => {
               <div className={styles.containPrice}>
                 <p>$282000</p>
                 <div>
-                <span>Off</span>
+                  <span>Off</span>
                   <p>%10</p>
                 </div>
               </div>
@@ -161,7 +252,7 @@ const Products = () => {
               <div className={styles.containPrice}>
                 <p>$282000</p>
                 <div>
-                <span>Off</span>
+                  <span>Off</span>
                   <p>%10</p>
                 </div>
               </div>
@@ -180,30 +271,6 @@ const Products = () => {
                 <p className={styles.btnViewMore}>Ver +</p>
               </div>
             </div>
-            <div className={styles.cardProduct}>
-              <div className={styles.containPrice}>
-                <p>$282000</p>
-                <div>
-                <span>Off</span>
-                  <p>%10</p>
-                </div>
-              </div>
-              <p>
-                MacBook Air 13.3 Apple M1 8GB 512GB SSD MacOS X 11 Space Gray
-              </p>
-              <div className={styles.center}>
-                <div
-                  className={styles.photo}
-                  style={{
-                    backgroundImage: `url("https://home.ripley.com.pe/Attachment/WOP_5/2004209413829/2004209413829-1.jpg")`,
-                  }}
-                ></div>
-              </div>
-              <div className={styles.center}>
-                <p className={styles.btnViewMore}>Ver +</p>
-              </div>
-            </div>
-            
           </div>
         </div>
       </div>
