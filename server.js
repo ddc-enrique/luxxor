f6b422e0d931d2a72efcf14e9440a6e6c2d2994b
enrique-dc93 <51374100+enrique-dc93@users.noreply.github.com>
2021-10-08 18:31:15 -0300

    primer commit para hostear

--- server.js
+++ server.js
@@ -15,5 +15,11 @@ app.use(express.json())
 app.use(fileupload())
 
 app.use("/api", router)
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname+'/client/build/index.html'))
+    })
+}
 
-app.listen(4000, () => console.log("Hello, the server is listening on port 4000"))
+app.listen(process.env.PORT || 4000, process.env.HOST || '0.0.0.0' , () => console.log("Hello, the server is listening on port 4000"))
