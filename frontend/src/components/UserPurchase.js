2a9a86558dddb7994d54edda5408196d416b0800
Rodrigo <goitiarodrigo@hotmail.com>
2021-10-12 15:36:36 -0300

    xxx

--- frontend/src/components/UserPurchase.js
+++ frontend/src/components/UserPurchase.js
@@ -9,32 +9,67 @@ import { Link } from "react-router-dom"
 const UserPurchase = (props) => {
     //https://i.postimg.cc/ZRKk8wv4/nh-2-removebg-preview-1.png
     const [myProducts, setMyProducts] = useState()
-
+    const [myShoppingDetails, setMyShoppingDetails] = useState()
     const [error, setError] = useState("")
 
     useEffect(()=> {
        
        const myShops = async () => {
        let response = await props.myShopping(props.userId)
-       console.log(response)
         if(!response.success) setError(response.response)
-            
+        setMyProducts(response)
+        
        }
        myShops()
     }, [])
+    console.log()
+    let myShopping = []
+    let sales = []
+
+    if (myProducts && !error){
+        myShopping = myProducts.map(product=> product.shopCart)
+        myShopping.map(shop => shop.forEach(shop => sales.push(shop)))
+    }
 
 
     return (
-        <div>
+        <>
+            {!props.view && 
+            <>
+            <h2 className={styles.title}>Historial de compras</h2>
+            <nav className={styles.navegator}>
+                <Link  to="#">Mis compras</Link> 
+                <Link onClick={()=>props.setView(true)} to="#">Mis datos</Link>
+            </nav>
+            </>
+            }
+            
             <div className={!props.view ? styles.divContainer : styles.none}>
-                        <Link  to="#">Mis compras</Link> 
-                        <Link onClick={()=>props.setView(true)} to="#">Mis datos</Link>
-                <div style={{backgroundImage: `url(${"https://i.postimg.cc/ZRKk8wv4/nh-2-removebg-preview-1.png"})`}}></div>
-                <div>
 
+                {error ? <div>
+                    <div>
+                        <h1>No tienes compras realizadas</h1>
+                        <h2>Puedes comenzar a comprar haciendo click <Link to="/productos">AQUÍ</Link></h2>
+                    </div>
+                    <div className={styles.photoShop} style={{backgroundImage: `url(${"https://i.postimg.cc/MKvCYWqL/prod.png"})`, width: "30rem", height: "30rem"}}></div>
                 </div>
+                : <div className={styles.productsContainer}>
+                    {sales && sales.map(sale => {
+                        return (
+                            <div className={styles.container}>
+                                <div className={styles.divImage} style={{backgroundImage: `url("http://localhost:4000/productsPhoto/${sale.productId.photos[0]}")`, height: "10rem", width: "10rem"}}>
+                                </div>
+                                <div>
+                                    <p>{sale.productId.name}</p>
+                                    <p>{sale.quantity}</p>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>}
+                
             </div>
-        </div>
+        </>
     )
 }
 
